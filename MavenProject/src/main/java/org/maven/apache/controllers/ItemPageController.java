6a986c4f1b89b9b31d2c4388cd1937da9abd207e
HaoxuanGong <hgon777@aucklanduni.ac.nz>
2022-11-26 12:44:45 +1300

    Format and Clean Up

--- MavenProject/src/main/java/org/maven/apache/controllers/ItemPageController.java
+++ MavenProject/src/main/java/org/maven/apache/controllers/ItemPageController.java
@@ -9,7 +9,7 @@ import org.maven.apache.item.Item;
 import org.maven.apache.item.ItemFX;
 import org.maven.apache.item.ItemFXBuilder;
 import org.maven.apache.mapper.ItemMapper;
-import org.maven.apache.mybatis.MyBatisConnector;
+
 import org.maven.apache.mybatis.MyBatisItemConnector;
 
 import com.jfoenix.controls.JFXTreeTableColumn;
@@ -22,81 +22,79 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
+
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 import javafx.util.Callback;
 
 public class ItemPageController implements Initializable {
 
-	@FXML
-	private JFXTreeTableView<ItemFX> itemTable;
+    @FXML
+    private JFXTreeTableView<ItemFX> itemTable;
 
-	@FXML
-	private JFXTreeTableColumn<ItemFX, String> idColumn = new JFXTreeTableColumn("ID");
+    @FXML
+    private final JFXTreeTableColumn<ItemFX, String> idColumn = new JFXTreeTableColumn("ID");
 
-	@FXML
-	private JFXTreeTableColumn<ItemFX, String> nameColumn = new JFXTreeTableColumn("Item Name");;
+    @FXML
+    private final JFXTreeTableColumn<ItemFX, String> nameColumn = new JFXTreeTableColumn("Item Name");
 
-	@FXML
-	private JFXTreeTableColumn<ItemFX, String> unitColumn = new JFXTreeTableColumn("Unit");;
+    @FXML
+    private final JFXTreeTableColumn<ItemFX, String> unitColumn = new JFXTreeTableColumn("Unit");
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		
-		idColumn.setPrefWidth(100);
-		idColumn.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<ItemFX, String>, ObservableValue<String>>(){
-			
-			@Override
-			public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<ItemFX, String> param){
-				return param.getValue().getValue().getID();
-			}
-		});
-		
-		nameColumn.setPrefWidth(300);
-		nameColumn.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<ItemFX, String>, ObservableValue<String>>(){
-			
-			@Override
-			public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<ItemFX, String> param){
-				return param.getValue().getValue().getName();
-			}
-		});
-		
-		unitColumn.setPrefWidth(100);
-		unitColumn.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<ItemFX, String>, ObservableValue<String>>(){
-			
-			@Override
-			public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<ItemFX, String> param){
-				return param.getValue().getValue().getUnit();
-			}
-		});
-		
-		ObservableList<ItemFX> items = FXCollections.observableArrayList();
-		
-		try {
-			List<ItemFX> ItemFXList = getList();
-			for(ItemFX item : ItemFXList) {
-				items.add(item);
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		final TreeItem<ItemFX> root = new RecursiveTreeItem<ItemFX>(items, RecursiveTreeObject::getChildren);
-		itemTable.getColumns().setAll(idColumn, nameColumn, unitColumn);
-		itemTable.setRoot(root);
-		itemTable.setShowRoot(false);
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
-	
+        idColumn.setPrefWidth(100);
+        idColumn.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<ItemFX, String>, ObservableValue<String>>() {
+
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<ItemFX, String> param) {
+                return param.getValue().getValue().getID();
+            }
+        });
+
+        nameColumn.setPrefWidth(300);
+        nameColumn.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<ItemFX, String>, ObservableValue<String>>() {
+
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<ItemFX, String> param) {
+                return param.getValue().getValue().getName();
+            }
+        });
+
+        unitColumn.setPrefWidth(100);
+        unitColumn.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<ItemFX, String>, ObservableValue<String>>() {
+
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<ItemFX, String> param) {
+                return param.getValue().getValue().getUnit();
+            }
+        });
+
+        ObservableList<ItemFX> items = FXCollections.observableArrayList();
+
+        try {
+            List<ItemFX> ItemFXList = getList();
+            for (ItemFX item : ItemFXList) {
+                items.add(item);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        final TreeItem<ItemFX> root = new RecursiveTreeItem<ItemFX>(items, RecursiveTreeObject::getChildren);
+        itemTable.getColumns().setAll(idColumn, nameColumn, unitColumn);
+        itemTable.setRoot(root);
+        itemTable.setShowRoot(false);
 
 
-	}
+    }
 
-	private List<ItemFX> getList() throws IOException {
-		ItemMapper itemMapper = MyBatisItemConnector.getItemMapper();
-		List<Item> items = itemMapper.selectAll();
-		return ItemFXBuilder.buildList(items);
-	}
+    private List<ItemFX> getList() throws IOException {
+        ItemMapper itemMapper = MyBatisItemConnector.getItemMapper();
+        List<Item> items = itemMapper.selectAll();
+        return ItemFXBuilder.buildList(items);
+    }
 
 }
