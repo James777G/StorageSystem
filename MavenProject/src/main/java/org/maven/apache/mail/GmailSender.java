67beccc7d74d9f837b09d02b0cce7216b7ec66d8
anthonyffeng <yfen795@aucklanduni.ac.nz>
2022-12-06 21:56:14 +1300

    Use Gmail API to Authenticate google account

---
+++ MavenProject/src/main/java/org/maven/apache/mail/GmailSender.java
@@ -0,0 +1,119 @@
+package org.maven.apache.mail;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Message;
+import org.apache.commons.codec.binary.Base64;
+import org.testng.annotations.Test;
+
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.Set;
+
+import static com.google.api.services.gmail.GmailScopes.*;
+import static javax.mail.Message.RecipientType.TO;
+
+public class GmailSender {
+
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @param httpTransport The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+
+    private static final String Test_Mail = "javamailsender.version2@gmail.com";
+    private static final String Receiver_Mail = "jamesgong0719@gmail.com";
+    private final Gmail service;
+
+    public GmailSender() throws Exception {
+        NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+        service = new Gmail.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
+                .setApplicationName("Verification Code Sender")
+                .build();
+    }
+
+    public void sendVerificationCode(String subject, String message) throws Exception {
+        // send verification code
+
+
+
+        // Encode as MIME message
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage email = new MimeMessage(session);
+        email.setFrom(new InternetAddress(Test_Mail));
+        email.addRecipient(TO, new InternetAddress(Receiver_Mail));
+        email.setSubject(subject);
+        email.setText(message);
+
+        // Encode and wrap the MIME message into a gmail message
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        email.writeTo(buffer);
+        byte[] rawMessageBytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
+        Message msg = new Message();
+        msg.setRaw(encodedEmail);
+
+        try {
+            // Create send message
+            msg = service.users().messages().send("me", msg).execute();
+            System.out.println("Message id: " + msg.getId());
+            System.out.println(msg.toPrettyString());
+        } catch (GoogleJsonResponseException e) {
+            // TODO(developer) - handle error appropriately
+            GoogleJsonError error = e.getDetails();
+            if (error.getCode() == 403) {
+                System.err.println("Unable to send message: " + e.getDetails());
+            } else {
+                throw e;
+            }
+        }
+    }
+    private static Credential getCredentials(final NetHttpTransport httpTransport,GsonFactory jsonFactory)
+            throws IOException {
+        // Load client secrets.
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(jsonFactory, new InputStreamReader(GmailSender.class.getResourceAsStream("/gmail/client_secret_871291228520-np8foq9jkeqigt21bcstbu9miae17td8.apps.googleusercontent.com.json")));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, jsonFactory, clientSecrets, Set.of(GMAIL_SEND))
+                .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    @Test
+    public static void main(String[] args) throws Exception {
+        new GmailSender().sendVerificationCode("这封邮件使用了gmail api 和 javamail api", """
+                                            亲爱的读者，
+                                            
+                                               这是一封测试邮件。
+                                               
+                                            最好的祝福，
+                                            仓库系统团队
+                                            """);
+    }
+}
+
