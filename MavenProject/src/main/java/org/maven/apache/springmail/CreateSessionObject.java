25087c47bac31f2edb13118439ada47598e517ef
anthonyffeng <yfen795@aucklanduni.ac.nz>
2022-12-11 13:30:20 +1300

    Add CreateSessionObject

--- MavenProject/src/main/java/org/maven/apache/springmail/CreateSessionObject.java
+++ MavenProject/src/main/java/org/maven/apache/springmail/CreateSessionObject.java
@@ -1,2 +1,28 @@
-package org.maven.apache.springmail;public class CreateSessionObject {
+package org.maven.apache.springmail;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+
+import java.util.Properties;
+
+public class CreateSessionObject {
+
+    private Session generateSession(){
+        Properties properties = new Properties();
+        properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.port", "587");
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("javamail.springboot@gmail.com","wjwbizfjurjknukb");
+            }
+        });
+
+        return session;
+    }
+
+
 }
