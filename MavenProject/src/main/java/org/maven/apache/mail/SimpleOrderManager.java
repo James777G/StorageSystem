0b07d3632f78c471d5526a69614f0f62097a5757
HaoxuanGong <hgon777@aucklanduni.ac.nz>
2022-12-12 14:50:28 +1300

    update mail verification system

--- MavenProject/src/main/java/org/maven/apache/mail/SimpleOrderManager.java
+++ MavenProject/src/main/java/org/maven/apache/mail/SimpleOrderManager.java
@@ -1,35 +1,18 @@
 package org.maven.apache.mail;
 
+import lombok.Data;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 @Service
+@Data
 public class SimpleOrderManager{
-
 
     private MailSender mailSender;
 
     private SimpleMailMessage templateMessage;
-
-    public MailSender getMailSender() {
-        return mailSender;
-    }
-
-    public void setMailSender(MailSender mailSender) {
-        this.mailSender = mailSender;
-    }
-
-    public SimpleMailMessage getTemplateMessage() {
-        return templateMessage;
-    }
-
-    public void setTemplateMessage(SimpleMailMessage templateMessage) {
-        this.templateMessage = templateMessage;
-    }
-
-
 
     public void placeOrder(String recipientEmail, String text) {
         templateMessage.setTo(recipientEmail);
