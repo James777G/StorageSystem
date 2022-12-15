5d2744907b7062135d8423b9861aef0c0296a17c
HaoxuanGong <hgon777@aucklanduni.ac.nz>
2022-12-15 18:37:01 +1300

    update Scene

--- MavenProject/src/main/java/org/maven/apache/App.java
+++ MavenProject/src/main/java/org/maven/apache/App.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -39,6 +40,7 @@ public class App extends Application {
 	  public void start(final Stage stage) throws IOException {
 		  
 	    final Scene scene = new Scene(loadFxml("appPage"));
+		scene.setFill(Color.TRANSPARENT);
 	    stage.setScene(scene);
 	    stage.initStyle(StageStyle.TRANSPARENT);
 	    stage.show();
