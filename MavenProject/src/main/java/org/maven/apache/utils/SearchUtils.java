7be1ee1112b8dcc47ecbf8ffac24ad2461f4c725
HaoxuanGong <hgon777@aucklanduni.ac.nz>
2022-12-26 17:59:14 +1300

    added cas

--- MavenProject/src/main/java/org/maven/apache/utils/SearchUtils.java
+++ MavenProject/src/main/java/org/maven/apache/utils/SearchUtils.java
@@ -18,7 +18,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class SearchUtils {
 
-    private static AtomicInteger atomicInteger= new AtomicInteger();
+    private static final AtomicInteger atomicInteger= new AtomicInteger();
 
     private static final ExecutorService executorService = MyLauncher.context.getBean("threadPoolExecutor", ExecutorService.class);
     private static final ItemService itemService = MyLauncher.context.getBean("itemService", ItemService.class);
