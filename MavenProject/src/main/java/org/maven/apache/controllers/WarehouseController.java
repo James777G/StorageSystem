320c8f6f070306f1f6d5a6b7a01c4aaf098e96f5
HaoxuanGong <hgon777@aucklanduni.ac.nz>
2023-01-12 23:41:02 +1300

    merge in intellij warehouse page basically done

--- MavenProject/src/main/java/org/maven/apache/controllers/WarehouseController.java
+++ MavenProject/src/main/java/org/maven/apache/controllers/WarehouseController.java
@@ -2,10 +2,13 @@ package org.maven.apache.controllers;
 
 import com.jfoenix.controls.JFXButton;
 import io.github.palexdev.materialfx.controls.MFXPagination;
+import io.github.palexdev.materialfx.dialogs.MFXGenericDialog;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import org.maven.apache.MyLauncher;
 import org.maven.apache.item.Item;
 import org.maven.apache.service.item.ItemService;
@@ -15,6 +18,8 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class WarehouseController implements Initializable {
 
@@ -35,7 +40,25 @@ public class WarehouseController implements Initializable {
     private JFXButton checkOne, checkTwo, checkThree, checkFour, checkFive, checkSix, checkSeven;
 
     @FXML
+    private TextField itemNameInDetails;
+
+    @FXML
+    private Label itemIdInDetails;
+
+    @FXML
+    private TextField itemAmountInDetails;
+
+    @FXML
+    private TextArea itemDescriptionInDetails;
+
+    @FXML
     private MFXPagination pagination;
+
+    @FXML
+    private MFXGenericDialog descriptionDialog;
+
+    @FXML
+    private Label warnMessage;
 
     private int pageSize;
 
@@ -49,9 +72,13 @@ public class WarehouseController implements Initializable {
 
     private final JFXButton[] buttonList = new JFXButton[7];
 
+    private Item selectedItem;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        descriptionDialog.setVisible(false);
+        warnMessage.setVisible(false);
         calculatePageSize();
         initializeNameList();
         initializeIdList();
@@ -62,15 +89,115 @@ public class WarehouseController implements Initializable {
         initializeItemList();
         setTableContents();
         pagination.currentPageProperty().addListener((observable, oldValue, newValue) -> executorService.execute(() -> {
-            List<Item> items = DataUtils.publicCachedWarehouseTableData.get(newValue.intValue() - 1);
-            setItemList(items);
+            generateItemList(newValue.intValue() - 1);
             Platform.runLater(WarehouseController.this::setTableContents);
         }));
+    }
+
+    @FXML
+    private void onClickCheckOne() {
+        if (itemList[0] != null) {
+            setItemAttributes(itemList[0]);
+        }
+    }
+
+    @FXML
+    private void onClickCheckTwo() {
+        if (itemList[1] != null) {
+            setItemAttributes(itemList[1]);
+        }
+    }
+
+    @FXML
+    private void onClickCheckThree() {
+        if (itemList[2] != null) {
+            setItemAttributes(itemList[2]);
+        }
+    }
+
+    @FXML
+    private void onClickCheckFour() {
+        if (itemList[3] != null) {
+            setItemAttributes(itemList[3]);
+        }
+    }
+
+    @FXML
+    private void onClickCheckFive() {
+        if (itemList[4] != null) {
+            setItemAttributes(itemList[4]);
+        }
+    }
+
+    @FXML
+    private void onClickCheckSix() {
+        if (itemList[5] != null) {
+            setItemAttributes(itemList[5]);
+        }
+    }
+
+    @FXML
+    private void onClickCheckSeven() {
+        if (itemList[6] != null) {
+            setItemAttributes(itemList[6]);
+        }
+    }
+
+    @FXML
+    private void onCloseDescriptionDialog() {
+        descriptionDialog.setVisible(false);
+        warnMessage.setVisible(false);
+    }
+
+    /**
+     * need improvements
+     */
+    @FXML
+    private void onClickApply() {
+        try{
+            Item item = encapsulateItemData();
+            itemService.update(item);
+            generateCachedData();
+            int currentPage = pagination.getCurrentPage() - 1;
+            generateItemList(currentPage);
+            setTableContents();
+        }catch(Exception e){
+            warnMessage.setVisible(true);
+        }
+    }
+
+    private boolean isItemNameAlreadyExisted() {
+        for (int i = 0; i < DataUtils.publicCachedWarehouseTableData.size(); i++) {
+            for (int j = 0; j < DataUtils.publicCachedWarehouseTableData.get(i).size(); j++) {
+                if (DataUtils.publicCachedWarehouseTableData.get(i).get(j).getItemName().equals(itemNameInDetails.getText())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private Item encapsulateItemData() {
+        Item item = new Item();
+        item.setItemID(Integer.parseInt(itemIdInDetails.getText()));
+        item.setItemName(itemNameInDetails.getText());
+        item.setUnit(Integer.parseInt(itemAmountInDetails.getText()));
+        item.setDescription(itemDescriptionInDetails.getText());
+        return item;
     }
 
     private void initializeItemList() {
         List<Item> items = DataUtils.publicCachedWarehouseTableData.get(0);
         setItemList(items);
+    }
+
+    private void setItemAttributes(Item item) {
+        descriptionDialog.setVisible(true);
+        itemNameInDetails.setText(item.getItemName());
+        itemDescriptionInDetails.setText(item.getDescription());
+        itemAmountInDetails.setText(item.getUnit().toString());
+        itemIdInDetails.setText(item.getItemID().toString());
+        selectedItem = item;
     }
 
     /**
@@ -138,11 +265,18 @@ public class WarehouseController implements Initializable {
      *
      * @param items calculated items (from cached data)
      */
+    @Deprecated
+    @SuppressWarnings("all")
     private void setItemList(List<Item> items) {
         itemList = new Item[7];
         for (int i = 0; i < items.size(); i++) {
             itemList[i] = items.get(i);
         }
+    }
+
+    private void generateItemList(int index) {
+        List<Item> items = DataUtils.publicCachedWarehouseTableData.get(index);
+        setItemList(items);
     }
 
     /**
@@ -193,7 +327,7 @@ public class WarehouseController implements Initializable {
      * 3. The cached data generated must be cleared and regenerated everytime when there is a change to the data
      * 4. The cached data is stored as a global variable in DataUtils class.
      */
-    private void generateCachedData() {
+    public void generateCachedData() {
         calculatePageSize();
         DataUtils.publicCachedWarehouseTableData.clear();
         for (int i = 1; i <= pageSize; i++) {
