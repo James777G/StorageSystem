029db45b461a8ba2d8fb9d486a9575e70fc921d0
HaoxuanGong <hgon777@aucklanduni.ac.nz>
2023-01-18 13:20:39 +1300

    commit

--- MavenProject/src/main/java/org/maven/apache/controllers/StaffController.java
+++ MavenProject/src/main/java/org/maven/apache/controllers/StaffController.java
@@ -1,15 +1,59 @@
 package org.maven.apache.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import io.github.palexdev.materialfx.controls.MFXProgressSpinner;
+import io.github.palexdev.materialfx.dialogs.MFXGenericDialog;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StaffController implements Initializable {
 
+    @FXML
+    private Label staffNameOne, staffNameTwo, staffNameThree, staffNameFour, staffNameFive, staffNameSix, staffNameSeven;
+
+    @FXML
+    private Label staffIdOne, staffIdTwo, staffIdThree, staffIdFour, staffIdFive, staffIdSix, staffIdSeven;
+
+    @FXML
+    private Label staffStatusOne, staffStatusTwo, staffStatusThree, staffStatusFour, staffStatusFive, staffStatusSix, staffStatusSeven;
+
+    @FXML
+    private JFXButton editOne, editTwo, editThree, editFour, editFive, editSix, editSeven;
+
+    @FXML
+    private MFXGenericDialog descriptionDialog;
+
+    @FXML
+    private MFXProgressSpinner loadSpinner;
+
+    private Label[] nameList = new Label[7];
+    private Label[] idList = new Label[7];
+    private Label[] statusList = new Label[7];
+    private JFXButton[] buttonList = new JFXButton[7];
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        loadSpinner.setVisible(false);
+        initializeNameList();
+    }
 
+    public void initializeNameList(){
+        nameList[0] = staffNameOne;
+        nameList[1] = staffNameTwo;
+        nameList[2] = staffNameThree;
+        nameList[3] = staffNameFour;
+        nameList[4] = staffNameFive;
+        nameList[5] = staffNameSix;
+        nameList[6] = staffNameSeven;
+    }
+
+    @FXML
+    private void onCloseDescription(){
+        descriptionDialog.setVisible(false);
+        loadSpinner.setVisible(false);
     }
 }
