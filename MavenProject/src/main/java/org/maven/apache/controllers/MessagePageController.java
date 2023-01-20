7dd5450d317447f8d634debb99b53ff34986efbb
anthonyffeng <yfen795@aucklanduni.ac.nz>
2023-01-20 22:43:31 +1300

    Add css in message Page

---
+++ MavenProject/src/main/java/org/maven/apache/controllers/MessagePageController.java
@@ -0,0 +1,32 @@
+package org.maven.apache.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MessagePageController implements Initializable {
+
+    @FXML
+    private AnchorPane notePadPane1;
+
+    @FXML
+    private AnchorPane notePadPane2;
+    @FXML
+    private AnchorPane notePadPane3;
+
+    @FXML
+    private AnchorPane notePadPane4;
+
+    @FXML
+    private AnchorPane notePadPane5;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+}
