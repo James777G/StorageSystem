005f1354a0d801b631786122caee95f18407248d
anthonyffeng <yfen795@aucklanduni.ac.nz>
2023-01-20 23:09:03 +1300

    Finish green moving line animation in messagePage

--- MavenProject/src/main/java/org/maven/apache/controllers/MessagePageController.java
+++ MavenProject/src/main/java/org/maven/apache/controllers/MessagePageController.java
@@ -1,8 +1,10 @@
 package org.maven.apache.controllers;
 
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import org.maven.apache.utils.TranslateUtils;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +16,7 @@ public class MessagePageController implements Initializable {
 
     @FXML
     private AnchorPane notePadPane2;
+
     @FXML
     private AnchorPane notePadPane3;
 
@@ -23,10 +26,50 @@ public class MessagePageController implements Initializable {
     @FXML
     private AnchorPane notePadPane5;
 
+    @FXML
+    private AnchorPane onMoveToMessagePane;
+
+    @FXML
+    private AnchorPane onMoveToStarredPane;
+
+    @FXML
+    private AnchorPane movingLinePane;
+
+    private boolean isMovingLineOnMessage = true;
+
+    private boolean isLineMoving = false;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
+    @FXML
+    private void onClickMessage(){
+        if((!isMovingLineOnMessage) && (!isLineMoving)){
+            isLineMoving = true;
+            TranslateTransition translateTransition = TranslateUtils.getTranslateTransitionByX(movingLinePane, 500, -120);
+            translateTransition = TranslateUtils.addEaseOutTranslateInterpolator(translateTransition);
+            translateTransition.setOnFinished(event -> {
+                isLineMoving = false;
+                isMovingLineOnMessage = true;
+            });
+            translateTransition.play();
+        }
+    }
+
+    @FXML
+    private void onClickStarred(){
+        if((isMovingLineOnMessage) && (!isLineMoving)){
+            isLineMoving = true;
+            TranslateTransition translateTransition = TranslateUtils.getTranslateTransitionByX(movingLinePane, 500, 120);
+            translateTransition = TranslateUtils.addEaseOutTranslateInterpolator(translateTransition);
+            translateTransition.setOnFinished(event -> {
+                isLineMoving = false;
+                isMovingLineOnMessage = false;
+            });
+            translateTransition.play();
+        }
+    }
 
 }
