873c602ca33965371e1c25b274ff74a1257eccb0
anthonyffeng <yfen795@aucklanduni.ac.nz>
2023-01-24 17:01:02 +1300

    Change menuPage by Peter's thought

--- MavenProject/src/main/java/org/maven/apache/controllers/MenuPageController.java
+++ MavenProject/src/main/java/org/maven/apache/controllers/MenuPageController.java
@@ -6,6 +6,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import org.maven.apache.App;
 import org.maven.apache.utils.DataUtils;
@@ -14,12 +17,61 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class MenuPageController{
+public class MenuPageController implements Initializable{
 
     @FXML
     private JFXButton signOffButton;
+
+    @FXML
+    private JFXButton settingButton;
+
+    @FXML
+    private ImageView settingImageView;
+
+    @FXML
+    private ImageView logOutImageView;
+
+    @FXML
+    private final Image onEnterLogOutImage = new Image(Objects.requireNonNull(AppPage2Controller.class.getResourceAsStream("/image/icons8-logout-64 (1).png")));
+
+    @FXML
+    private final Image onEnterSettingImage = new Image(Objects.requireNonNull(AppPage2Controller.class.getResourceAsStream("/image/icons8-settings-192.png")));
+
+    @FXML
+    private final Image onExitLogOutImage = new Image(Objects.requireNonNull(AppPage2Controller.class.getResourceAsStream("/image/icons8-logout-64.png")));
+
+    @FXML
+    private final Image onExitSettingImage = new Image(Objects.requireNonNull(AppPage2Controller.class.getResourceAsStream("/image/icons8-settings-384.png")));
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        settingImageView.setImage(onExitSettingImage);
+        logOutImageView.setImage(onExitLogOutImage);
+    }
+
+    @FXML
+    private void onEnterSetting(){
+        settingImageView.setImage(onEnterSettingImage);
+    }
+
+    @FXML
+    private void onExitSetting(){
+        settingImageView.setImage(onExitSettingImage);
+    }
+
+    @FXML
+    private void onEnterLogOut(){
+        logOutImageView.setImage(onEnterLogOutImage);
+    }
+
+    @FXML
+    private void onExitLogOut(){
+        logOutImageView.setImage(onExitLogOutImage);
+    }
+
 
     @FXML
     private void onSignOff() throws IOException {
@@ -38,5 +90,7 @@ public class MenuPageController{
         onClickSettingsTwo.invoke(clazz, DataUtils.publicSettingsDialog);
         DataUtils.publicSettingBlockPane.setVisible(true);
     }
+
+
 }
 
