cbacad21ec47840805fd68012e3287b06f0c76e1
xxu511 <100254883+xxu511@users.noreply.github.com>
2023-02-14 19:53:07 +1300

    the message are showed on the labels

--- MavenProject/src/main/java/org/maven/apache/controllers/MessagePageController.java
+++ MavenProject/src/main/java/org/maven/apache/controllers/MessagePageController.java
@@ -1,5 +1,6 @@
 package org.maven.apache.controllers;
 
+import io.github.palexdev.materialfx.controls.MFXPagination;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,21 +8,29 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.Pagination;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
+import org.aspectj.bridge.MessageUtil;
 import org.maven.apache.MyLauncher;
+import org.maven.apache.message.Message;
 import org.maven.apache.service.item.CachedItemService;
 import org.maven.apache.service.message.CachedMessageService;
 import org.maven.apache.service.message.CachedMessageServiceProvider;
 import org.maven.apache.utils.DataUtils;
+import org.maven.apache.utils.MessageCachedUtils;
 import org.maven.apache.utils.TranslateUtils;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
 
 public class MessagePageController implements Initializable {
+
 
     @FXML
     private AnchorPane notePadPane1;
@@ -50,14 +59,39 @@ public class MessagePageController implements Initializable {
     private Pagination newPagination;
 
     @FXML
+    private Label staffName1,staffName2,staffName3,staffName4,staffName5;
+
+    @FXML
+    private Label category1,category2,category3,category4,category5;
+
+    @FXML
+    private Label time1,time2,time3,time4,time5;
+
+    @FXML
+    private Label message1,message2,message3,message4,message5;
+
+    @FXML
     private  final AnchorPane editMessagePane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/editMessagePage.fxml")));
 
     private final CachedMessageService cachedMessageService = MyLauncher.context.getBean("cachedMessageService", CachedMessageService.class);
 
     private final ExecutorService executorService = MyLauncher.context.getBean("threadPoolExecutor", ExecutorService.class);
 
+
+
     private Label[] staffNameArray= new Label[5];
 
+    private Label[] categoryArray= new Label[5];
+
+    private Label[] timeArray= new Label[5];
+
+    private Label[] messageArray = new Label[5];
+
+    private AnchorPane[] notePadPaneArray = new AnchorPane[5];
+
+    private List<List<Message>> messagePageList;
+
+    private List<Message> currentPageList;
     private boolean isMovingLineOnMessage = true;
 
     private boolean isLineMoving = false;
@@ -69,9 +103,101 @@ public class MessagePageController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         cachedMessageService.updateAllCachedMessageData();
 
-//        DataUtils.pagination=newPagination;
-//        newPagination.setMaxPageIndicatorCount(8);
+        initializeLabels();
 
+        setAllMessageContent();
+
+        newPagination.currentPageIndexProperty().addListener((observable, oldValue, newValue) -> {
+            updatePagination(newValue);
+        });
+
+    }
+
+
+
+    private void setAllMessageContent() {
+       messagePageList = MessageCachedUtils.getLists(MessageCachedUtils.listType.All_MESSAGE);
+       setContent();
+
+    }
+
+    private void setStarMessageContent(){
+        messagePageList = MessageCachedUtils.getLists(MessageCachedUtils.listType.STAR_MESSAGE);
+        setContent();
+    }
+
+    private void setContent(){
+        updatePagination(0);
+        newPagination.setCurrentPageIndex(0);
+        setPaginationPages(messagePageList);
+    }
+
+
+    private void setPaginationPages(List<List<Message>> messagePageList) {
+        newPagination.setPageCount(messagePageList.size());
+    }
+
+
+    private void updatePagination(Number currentPage) {
+       currentPageList = messagePageList.get(currentPage.intValue());
+
+        // set non-empty labels
+        for (int i = 0; i < currentPageList.size(); i++) {
+            staffNameArray[i].setText(currentPageList.get(i).getStaffName());
+            categoryArray[i].setText(currentPageList.get(i).getCategory());
+            timeArray[i].setText(dateText(currentPageList.get(i).getMessageTime()));
+            messageArray[i].setText(currentPageList.get(i).getInformation());
+           notePadPaneArray[i].setVisible(true);
+
+       }
+        // set empty labels
+        if (currentPageList.size() != 5) {
+            for (int j = 4; j >= currentPageList.size(); j--) {
+                notePadPaneArray[j].setVisible(false);
+            }
+        }
+
+
+    }
+
+    private String dateText(Date messageTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = sdf.format(messageTime);
+        return dateString;
+    }
+
+
+    private void initializeLabels() {
+        //initialize staffName Label
+        staffNameArray[0] =staffName1;
+        staffNameArray[1] =staffName2;
+        staffNameArray[2] =staffName3;
+        staffNameArray[3] =staffName4;
+        staffNameArray[4] =staffName5;
+        //initialize Category Label
+        categoryArray[0] = category1;
+        categoryArray[1] = category2;
+        categoryArray[2] = category3;
+        categoryArray[3] = category4;
+        categoryArray[4] = category5;
+        //initialize  Time Label
+        timeArray[0] = time1;
+        timeArray[1] = time2;
+        timeArray[2] = time3;
+        timeArray[3] = time4;
+        timeArray[4] = time5;
+        //initialize message label
+        messageArray[0] =message1;
+        messageArray[1] =message2;
+        messageArray[2] =message3;
+        messageArray[3] =message4;
+        messageArray[4] =message5;
+        //initialize anchorpane
+        notePadPaneArray[0] = notePadPane1;
+        notePadPaneArray[1] = notePadPane2;
+        notePadPaneArray[2] = notePadPane3;
+        notePadPaneArray[3] = notePadPane4;
+        notePadPaneArray[4] = notePadPane5;
     }
 
     @FXML
@@ -112,6 +238,16 @@ public class MessagePageController implements Initializable {
                 isMovingLineOnMessage = false;
             });
             translateTransition.play();
+        }
+    }
+
+    @FXML
+    private void onScrolled(ScrollEvent event){
+        if(event.getDeltaY() > 0){
+            newPagination.setCurrentPageIndex(newPagination.getCurrentPageIndex() + 1);
+        }
+        if(event.getDeltaY() < 0){
+            newPagination.setCurrentPageIndex(newPagination.getCurrentPageIndex() - 1);
         }
     }
 
--- MavenProject/src/main/resources/sql/message.sql
+++ MavenProject/src/main/resources/sql/message.sql
@@ -12,3 +12,24 @@ MessageTime date not null,
 Star int not null default 0
 
 );
+
+insert into MessageTable (StaffName,Category,Information,MessageTime)values
+('Tom','Item','NONE', curdate()),
+('Amy','Item','ddd',curdate()),
+('Smith','Staff','dfrW',curdate()),
+('Berry','Item','ddghr',curdate()),
+('Peter','Item','dijdd',curdate()),
+('Wang','Item','ddted',curdate());
+
+insert into MessageTable (StaffName,Category,Information,MessageTime,Star)values
+('Li','Item','iiuyuy',curdate(),1),
+('Ming','Item','45654',curdate(),1),
+('Tang','Item','845',curdate(),1),
+('Liu','Item','gvgfv',curdate(),1);
+
+insert into MessageTable (StaffName,Category,Information,MessageTime)values
+('Jess','Item','coffee Needed', curdate()),
+('Laura','Item','Sugar',curdate());
+
+
+
